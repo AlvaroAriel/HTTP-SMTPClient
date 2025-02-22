a17c7c4cbfda7884c94b4b684975d93dd615a458
AlvaroAriel <ariel57666@gmail.com>
2025-02-22 13:14:16 -0300

    feat: Moved WriteJSON function to new response package for api

---
+++ server/response.go
@@ -0,0 +1,12 @@
+package response
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func WriteJson(w http.ResponseWriter, statusCode int, v any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(v)
+}
