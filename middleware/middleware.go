ad8649e2ea491c72bf69960b16b6a396841669d8
AlvaroAriel <ariel57666@gmail.com>
2025-02-22 13:50:01 -0300

    feat: Allowed Hosts from enviroment variable

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,13 +1,16 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func CorsMiddleware(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		w.Header().Add("Access-Control-Allow-Origin", "*")
-		w.Header().Add("Access-Control-Allow-Headers", "origin,content-type")
+		w.Header().Add("Access-Control-Allow-Origin", os.Getenv("ALLOWED_HOSTS"))
+		w.Header().Add("Access-Control-Allow-Headers", "origin, content-type")
 		w.Header().Add("Access-Control-Allow-Methods", "POST")
 
 		if r.Method == "OPTIONS" {
