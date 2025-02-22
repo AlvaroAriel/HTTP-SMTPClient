0ccdd452d26c6ca8843a3f07da3f5a13b06f54a5
AlvaroAriel <ariel57666@gmail.com>
2025-02-22 13:40:18 -0300

    feat: Middleware package with Cors middleware functionality

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	httperror "github.com/AlvaroAriel/HTTP-SMTPClient/error"
+	"github.com/AlvaroAriel/HTTP-SMTPClient/middleware"
 	smtpclient "github.com/AlvaroAriel/HTTP-SMTPClient/smptclient"
 )
 
@@ -44,7 +45,7 @@ func main() {
 	mux.HandleFunc("POST /send", handleSendEmail(smtpClient, recipients))
 
 	fmt.Println("Starting server on port 8080...")
-	err = http.ListenAndServe(":8080", mux)
+	err = http.ListenAndServe(":8080", middleware.CorsMiddleware(mux))
 
 	if err != nil {
 		fmt.Println(fmt.Errorf("something went wrong while initializating the serve %w", err))
