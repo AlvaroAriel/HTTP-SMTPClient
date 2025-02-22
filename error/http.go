68531b6c4e39920a6fe7f7498d326c7db62038c8
AlvaroAriel <ariel57666@gmail.com>
2025-02-22 13:14:16 -0300

    feat: Moved WriteJSON function to new response package for api

--- error/http.go
+++ error/http.go
@@ -1,9 +1,10 @@
 package httperror
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
+
+	response "github.com/AlvaroAriel/HTTP-SMTPClient/server"
 )
 
 type APIError struct {
@@ -30,20 +31,14 @@ func EmptyField() APIError {
 	return NewAPIError(http.StatusBadRequest, fmt.Errorf("empty field"))
 }
 
-func WriteJson(w http.ResponseWriter, statusCode int, v any) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(statusCode)
-	json.NewEncoder(w).Encode(v)
-}
-
 func JSONError(w http.ResponseWriter, err error) {
 
 	if e, ok := err.(APIError); ok {
-		WriteJson(w, e.StatusCode, e)
+		response.WriteJson(w, e.StatusCode, e)
 		return
 	}
 
-	WriteJson(w, http.StatusInternalServerError, map[string]string{
+	response.WriteJson(w, http.StatusInternalServerError, map[string]string{
 		"detail": "Internal Server Error",
 	})
 }
